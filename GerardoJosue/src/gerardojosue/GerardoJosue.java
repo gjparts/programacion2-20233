c65bd73d9355910b6368a31ab67a743eec355e7f
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-09-20 10:56:59 -0600

    commit 2009202313246

---
+++ GerardoJosue/src/gerardojosue/GerardoJosue.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerardojosue;
+
+/**
+ *
+ * @author Gerardo Portillo
+ */
+public class GerardoJosue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //comentario de una linea
+        
+        /*comentario
+        de multiples
+        lineas*/
+        
+        //imprimir texto en la consola del sistema y pasar a una nueva linea
+        System.out.println("Hola UNAH");
+        System.out.println("Ingenieria en Sistemas");
+        //imprimir texto en la consola del sistema y no pasar a un nuevo renglon
+        System.out.print("Soy Gerardo Portillo");
+        System.out.print("Vivo en SPS");
+        System.out.println();
+        System.out.println("Otra linea");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Texto");
+        //usar caracteres de escape
+        System.out.print("Soy Estudiante\nVivo en SPS\n");
+        System.out.println("Es un dia soleado\nEs un dia bello.");
+        
+        System.out.println("Num\tNombre\tEdad");
+        System.out.println("1\tJosue\t11");
+        System.out.println("2\tViena\t14");
+        System.out.println("3\tIrene\t40");
+        
+        
+    }
+    
+}
