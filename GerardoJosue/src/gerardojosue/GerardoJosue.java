895b1937078c9059821544cc8ca887bdaf18f32e
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-09-21 11:56:45 -0600

    commit 2109202312190

--- GerardoJosue/src/gerardojosue/GerardoJosue.java
+++ GerardoJosue/src/gerardojosue/GerardoJosue.java
@@ -43,7 +43,41 @@ public class GerardoJosue {
         System.out.println("2\tViena\t14");
         System.out.println("3\tIrene\t40");
         
+        int a; //variable declarada sin valor de inicio
+        //System.out.println(a); //no se puede operar variables sin inicializar
+        a = 12;
+        System.out.println(a);
+        //variables declaradas con valor de inicio
+        int b = 17, c = 100, d = 88, e, f = 890;
+        e = 7; ///sobreescritura de variable
+        System.out.println("El valor de b es "+b);
+        System.out.println( "La suma de a y b es: "+(a+b) );
         
+        double d1 = 100.5;
+        double d2, d3 = 0.795412545;
+        double d4 = Math.PI;
+        double d5 = d1+d3;
+        System.out.println("El valor de d1 es "+d1);
+        System.out.println("El valor de d3 es "+d3);
+        System.out.println("El valor de d4 es "+d4);
+        System.out.println("El valor de d5 es "+d5);
+        
+        double d6 = Math.pow(2, 5);
+        System.out.println("2 elevado a la 5 potencia: "+d6);
+        d6 = Math.pow(d1, 2);
+        System.out.println(d1+" elevado al cuadrado es: "+d6);
+        d6 = Math.sqrt(d1);
+        System.out.println("La raiz cuadrada de "+d1+" es "+d6);
+        
+        float f1 = 3.1231321f;
+        float f2 = 4.0f, f3 = 4, f5 = f1*5;
+        System.out.println("El valor de f1 es "+f1);
+        System.out.println("El valor de f1 es "+f2);
+        System.out.println("El valor de f1 es "+f3);
+        System.out.println("El valor de f1 es "+f5);
+        
+        System.out.println( "La resta de "+f2+" menos "+f1+" es "+(f2-f1) );
+        System.out.println( "La division de "+f2+" entre "+f1+" es "+(f2/f1) );
     }
     
 }
