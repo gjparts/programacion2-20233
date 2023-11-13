8ace31adad7f80f6465b6a610feadbc803da085b
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-11-13 10:55:33 -0600

    commit 1311202311731

---
+++ 36_gui01/src/pkg36_gui01/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg36_gui01;
+
+/**
+ *
+ * @author Gerardo Portillo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //crear un nuevo objeto de clase Principal (instanciar)
+        Principal p1 = new Principal();
+        //mandar a dibujar el form en la pantalla
+        p1.setVisible(true);
+    }
+    
+}
