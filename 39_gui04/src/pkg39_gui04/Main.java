d3a05f4107615a0770a6dc2d402c130855835290
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-11-16 11:57:25 -0600

    commit 1611202318323

---
+++ 39_gui04/src/pkg39_gui04/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg39_gui04;
+
+/**
+ *
+ * @author Gerardo Portillo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal p1 = new Principal();
+        p1.setVisible(true);
+    }
+    
+}
