552e4810791ce96a41a762e2469af918dc937fc3
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-11-29 11:03:45 -0600

    commit 291120235691

---
+++ 43_gui08/src/pkg43_gui08/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg43_gui08;
+
+/**
+ *
+ * @author Gerardo Portillo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana z = new Ventana();
+        z.setVisible(true);
+    }
+    
+}
