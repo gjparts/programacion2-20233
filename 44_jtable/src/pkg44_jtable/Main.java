3c7540aa17da38d9838d74b0ed8607e1a977d66d
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-12-04 10:56:17 -0600

    commit 041220235934

---
+++ 44_jtable/src/pkg44_jtable/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg44_jtable;
+
+/**
+ *
+ * @author Gerardo Portillo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal x = new Principal();
+        x.setVisible(true);
+    }
+    
+}
